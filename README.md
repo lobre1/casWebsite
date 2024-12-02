6eaafa90bcf51fe5f3a067bce2b81f5a922f9ff3
lobre1 <calday0520@gmail.com>
2024-12-02 14:00:49 -0400

    Updated

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # casWebsite
+
+The sourcecode for the website I use to present my cas activities and responses located at charlescas.neocities.org
---
+++ modules/gallery/galleryScript.js
@@ -0,0 +1,44 @@
+let slideIndex = 1;
+let secondInterval = 2;
+let interval;
+
+showSlides(slideIndex);
+startInterval();
+
+function startInterval(){
+  interval = setInterval(function(){
+      plusSlides(1)
+      showSlides(slideIndex);
+    }, secondInterval*1000);
+}
+
+function restartInterval(){
+  clearInterval(interval);
+  startInterval();
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+  restartInterval();
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+  restartInterval();
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("gallerySlide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
